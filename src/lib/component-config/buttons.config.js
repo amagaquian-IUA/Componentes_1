edaf2959bd7891d6f456edbea9f267a39c7554c6
Angel Magaquian <angelmagakhian@gmail.com>
2026-03-14 20:35:46 -0300

    botones

---
+++ src/lib/component-config/buttons.config.js
@@ -0,0 +1,32 @@
+export const buttonConfig = [
+    {
+        variant: "btn-primary",
+        title: "Boton 1",
+        handle: () => { alert("click") }
+    },
+    {
+        variant: "btn-secondary",
+        title: "Boton 2",
+        handle: () => { alert("click") }
+    },
+    {
+        variant: "btn-danger",
+        title: "Boton 3",
+        handle: () => { alert("click") }
+    },
+    {
+        variant: "btn-warning",
+        title: "Boton 4",
+        handle: () => { alert("click") }
+    },
+    {
+        variant: "",
+        title: "",
+        handle: () => { alert("click") }
+    },
+    {
+        variant: "btn-warning",
+        title: "Boton 6",
+        handle: () => { alert("click") }
+    },
+]
