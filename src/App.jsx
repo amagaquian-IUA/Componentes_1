3bda6aa9350b94cabb60960e865c8e59d299d082
Angel Magaquian <angelmagakhian@gmail.com>
2026-03-14 21:17:40 -0300

    Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Button from './components/common/Button'
+import Input from './components/common/Input'
 import { buttonConfig } from './lib/component-config/buttons.config'
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
         )
         )
       }
+      <Input placeHolder={"Texto de prueba 2"} isActive={true} type={"text"} />
+      <Input placeHolder={"Texto de prueba 3"} isActive={false} type={"text"} />
+      <Input placeHolder={"Texto de prueba 4"} isActive={true} type={"tel"} />
+      <Input placeHolder={"Coloca un número"} isActive={true} type={"number"} />
     </>
   )
 }
